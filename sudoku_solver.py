0c97dad8c3424bab94cf6e66af94c42596dd84ff
Sprite <orichalcumforge@gmail.com>
2021-08-17 18:55:46 -0500

    finished sudoku solver

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,66 +1,95 @@
 '''
-1. Create check if num is valid at current slot after assignment
-2. Create recursive function that inputs board
-3. Check board for any unassigned slots. If true, cycle through 1-9 to check if any nums are valid
-4. If all slots have been assigned and conform to rules, then return True
+1. Find empty index 
+2. Attempt to place digits 1-9 in index
+3. Check if the digit is valid in current spot (yes = recursively repeat steps 1-3)
+   if not, reset spot and go back to previous index
+4. Once board is full, puzzle is complete
 
+
+To-do: 
+complete isValid function: subsections
+create visuals to show board being solved
+double check to make sure solve method is correct (can't do till isValid is finished)
 '''
 
 
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
+board = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 3, 0, 8, 5], [0, 0, 1, 0, 2, 0, 0, 0, 0], [0, 0, 0, 5, 0, 7, 0, 0, 0], [0, 0, 4, 0, 0, 0, 1, 0, 0], [0, 9, 0, 0, 0, 0, 0, 0, 0], [5, 0, 0, 0, 0, 0, 0, 7, 3], [0, 0, 2, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 4, 0, 0, 0, 9]]
 
-def isValid(board, row, column, num):
+
+# Need to figure out subgrid
+def is_valid(board, num, row, column):
 
     # Check Row
-    for i in range(9):
+    for i in range(len(board[0])):
         if board[row][i] == num:
             return False
 
     # Check Column
-    for i in range(9):
+    for i in range(len(board)):
         if board[i][column] == num:
             return False
     
     # Check Sub-grids
+    top_left_row = 3 * (row // 3)
+    top_left_column = 3 * (column // 3)
+
     for i in range(3):
         for j in range(3):
-            pass
+            if board[top_left_row + i][top_left_column + j] == num:
+                return False
 
+
+
+    return True
+
+
+# Should work, returns the row and column of empty slot
 def empty_slot(board):
-    for i in range(9):
-        for j in range(9):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
             if board[i][j] == 0:
-                return True
-    return False
+                return i,j
+    return -1,-1
+
+
 
 def solve(board):
-    row = 0
-    column = 0
+    
 
-    # Base Case
-    if not empty_slot(board):
-        return True
+    row, column = empty_slot(board)
+    
+    if row == -1: return True
+
+
+
 
     for num in range(1,10):
-        
-        
-        
-        if solve(board): return True # Number works
-        
-        board[row][column] == 0 # resets the slot to 0 if ^ did not work
+        if is_valid(board, num, row, column):
 
+            board[row][column] = num
 
+            if solve(board): return True
 
+            board[row][column] = 0
+
+            
 
 
     return False    # Should trigger backtracking
+
+def print_grid(board):
+    for i, row in enumerate(board):
+        for j, el in enumerate(row):
+            print(el if el != 0 else ' ', end = '|' if j == 2 or j == 5 else ' ')
+        print('\n-----+-----+-----' if i == 2 or i == 5 else '')
+    print()
+
+print_grid(board)
+if solve(board): print_grid(board)
+else: print('No Solution')
+
+
+
+
+
+
